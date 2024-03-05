65ac2fc625482728c3a129244c121f5c8a630536
IbrahemHafezMohammad <ibrahem.hafez.mohammad@gmail.com>
2024-03-05 23:18:17 +0400

    new views

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const rootDir = require('../util/path');
 
+const product = [];
+
 router.get("/add-product", (req, res, next) => {
     console.log("in add-product middleware");
     console.log("something ");
@@ -12,9 +14,10 @@ router.get("/add-product", (req, res, next) => {
   });
   
   router.post("/add-product", (req, res, next) => {
-    console.log(req.body);
+    product.push({title: req.body.title});
     res.redirect("/");
   });
 
-module.exports = router;
+exports.routes = router;
+exports.product = product;
 
--- routes/shop.js
+++ routes/shop.js
@@ -4,8 +4,11 @@ const path = require('path');
 const router = express.Router();
 
 const rootDir = require('../util/path');
+
+const adminData = require('./admin');
  
 router.get("/", (req, res, next) => {
+  console.log('shop.js', adminData.product);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
