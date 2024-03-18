8ffae620e202284654488d2d8e754cf0fbf2fd03
IbrahemHafezMohammad <ibrahem.hafez.mohammad@gmail.com>
2024-03-18 22:02:48 +0400

    add ejs to project

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('hbs', expressHbs.engine({extname: '.hbs', defaultLayout: false, layoutsDir: "views/layouts/"}));
-app.set('view engine', 'hbs');
+// app.engine('hbs', expressHbs.engine({
+//   extname: '.hbs',
+//   defaultLayout: "main-layout",
+//   layoutsDir: "views/layouts/"
+// }));
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');// this is the default (no need for this line)
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,7 +29,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found'})
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
 });
 
 app.listen(3000);
