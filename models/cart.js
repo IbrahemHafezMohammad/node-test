4a01d3e7c44de01c7f3e21e39969c45e262965c7
IbrahemHafezMohammad <ibrahem.hafez.mohammad@gmail.com>
2024-05-01 23:14:23 +0400

    refactor

--- models/cart.js
+++ models/cart.js
@@ -2,40 +2,33 @@ const fs = require('fs');
 const path = require('path');
 const rootDir = require('../util/path');
 
-const p = path.join(
-    rootDir,
-    'data',
-    'cart.json'
-);
+const p = path.join(rootDir, 'data', 'cart.json');
 
 module.exports = class Cart {
     static addProduct(id, productPrice) {
         fs.readFile(p, (err, fileContent) => {
             let cart = { products: [], totalPrice: 0 };
             if (!err) {
-                cart = JSON.parse(fileContent);
+                try {
+                    cart = JSON.parse(fileContent);
+                } catch (jsonErr) {
+                    console.error('Error parsing JSON:', jsonErr);
+                }
             }
-            console.log(cart.products);
-            const existingProductIndex = cart.products.find(prod => prod.id === id);
-            const existingProduct = cart.products[existingProductIndex];
-            let updatedProduct;
-            if (existingProduct) {
-                updatedProduct = { ...existingProduct };
-                updatedProduct.qty = updatedProduct.qty + 1;
-                // cart.products = [...cart.products];
-                cart.products[existingProductIndex] = updatedProduct;
-
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            if (existingProductIndex !== -1) {
+                const existingProduct = cart.products[existingProductIndex];
+                existingProduct.qty++;
             } else {
-                updatedProduct = { id: id, qty: 1 };
-                cart.products = [...cart.products, updatedProduct];
+                cart.products.push({ id: id, qty: 1 });
             }
-            cart.totalPrice = cart.totalPrice + productPrice;
+            cart.totalPrice += productPrice;
+
             fs.writeFile(p, JSON.stringify(cart), err => {
-                console.log(err);
+                if (err) {
+                    console.error('Error writing file:', err);
+                }
             });
         });
-
     }
-
-
-}
+};
